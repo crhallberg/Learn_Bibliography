91c77949bf1374011a5344a07f05fc1d12e747df
SravanthiVillanova <sadusumi@villanova.edu>
2016-11-02 10:23:11 -0400

    chnages to new_language

--- src/App/Action/NewLanguageAction.php
+++ src/App/Action/NewLanguageAction.php
@@ -38,36 +38,10 @@ class NewLanguageAction
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
     {
-		//$displaystr = "Coming Soon";
-		$sth = $this->adapter->query("select * from agenttype");
-		$rows = $sth->execute();
-	//var_dump($this);
-	
-	//new
-	//$sth = $this->adapter->query("select * from agent");
-		//$rows = $sth->execute();
 		
-       /* $sql =  new Sql($this->adapter);
-        $sth = $sql->Select('agent');
-		
-		$padapter = new DbSelect($sth,$this->adapter);
-		$paginator = new Paginator($padapter);
-		
-		$paginator->setDefaultItemCountPerPage(8);
-        $allItems = $paginator->getTotalItemCount();
-        $countPages = $paginator->count();
-
-        $p = $request->getAttribute('page', '1');*/
-		//$p = $request->getUri()->getQuery();		
-		//var_dump($p);
-	//new end
-	
-	//
-	//echo $_POST['de_newlang'];
-	$sql = new Sql($this->adapter);
-	$sth = $sql->insert('translate_language');
+/*	$sth = $sql->insert('translate_language');
 	$newData = array(
-	//'id'=>1,
+	'id'=>1,
 	'text_de'=> 'de1',
     'text_en'=> 'en1',
     'text_es'=> 'es1',
@@ -77,21 +51,20 @@ class NewLanguageAction
     );
 	$sth->values($newData);
 	$selectString = $sql->getSqlStringForSqlObject($sth);
-	$results = $this->adapter->query($selectString, Adapter::QUERY_MODE_EXECUTE);
+	$results = $this->adapter->query($selectString, Adapter::QUERY_MODE_EXECUTE);*/
 	//
 	
-	//
-    /*$sql = new Sql($this->dbAdapter);
-    $insert = $sql->insert('table');
-    $newData = array(
-    'col1'=> 'val1',
-    'col2'=> 'val2',
-    'col3'=> 'val3'
-    );
-    $insert->values($newData);
-    $selectString = $sql->getSqlStringForSqlObject($insert);
-    $results = $this->dbAdapter->query($selectString, Adapter::QUERY_MODE_EXECUTE);*/
-	//
+	$table = new \App\Db\Table\TranslateLanguage($this->adapter);
+	$table->insert([
+		'text_de'=> 'de1',
+		'text_en'=> 'en1',
+		'text_es'=> 'es1',
+		'text_fr'=> 'fr1',
+		'text_it'=> 'it1',
+		'text_nl'=> 'nl1',
+	]);
+	
+	var_dump($table);
 	
         return new HtmlResponse($this->template->render('app::new_language', ['rows' => $rows]));
     }
