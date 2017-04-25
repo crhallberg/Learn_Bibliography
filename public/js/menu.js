7f51600a20c272eb7cff2e467f4951f60432cec8
Chris Hallberg <crhallberg@gmail.com>
2017-04-25 14:54:56 -0400

    Localize CDN hosted vendor files. Add document.ready to menu.js.

--- public/js/menu.js
+++ public/js/menu.js
@@ -3,7 +3,7 @@ var lastmenu;
 
 function getId(id)
 {
-    if (document.getElementById){  
+    if (document.getElementById){
         return document.getElementById(id);
     } else if (document.all) {
         return document.all.id;
@@ -11,7 +11,7 @@ function getId(id)
         return document.id;
     }
 }
-  
+
 function hover(elem, id)
 {
     elem.style.backgroundColor='#6699CC';
@@ -40,7 +40,7 @@ function expand(id)
         elemImg = getId(id + 'img');
         elemImg.src = 'images/down.gif';
         lastmenu = elem;
-    }        
+    }
 }
 
 function hideAll()
@@ -50,15 +50,16 @@ function hideAll()
     }
 }
 
-$('.items a').on('click', function() {
-  var $this = $(this),
-      $bc = $('<div class="item"></div>');
+$(document).ready(function menuReady() {
+  $('.items a').on('click', function() {
+    var $this = $(this),
+        $bc = $('<div class="item"></div>');
 
-  $this.parents('li').each(function(n, li) {
-      var $a = $(li).children('a').clone();
-      $bc.prepend(' / ', $a);
-  });
-    $('.breadcrumb').html( $bc.prepend('<a href="#home">Home</a>') );
-    return false;
-}) 
-  
+    $this.parents('li').each(function(n, li) {
+        var $a = $(li).children('a').clone();
+        $bc.prepend(' / ', $a);
+    });
+      $('.breadcrumb').html( $bc.prepend('<a href="#home">Home</a>') );
+      return false;
+  })
+});
